111040c5b2e39e88cbb0216c93177cc3f2a1ed1b
Pavitra Kurseja <pavitra.kurseja@gmail.com>
2020-04-04 20:05:46 -0400

    Mutations

---
+++ data/dbConnectors.js
@@ -0,0 +1,69 @@
+import mongoose from 'mongoose';
+import Sequelize from 'sequelize';
+import _ from 'lodash';
+import casual from 'casual';
+
+// Mongo connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/friends', {
+    useMongoClient: true,
+    useFindAndModify: false
+});
+
+// create schema 
+const friendSchema = new mongoose.Schema({
+    firstName: {
+        type: String
+    },
+    lastName: {
+        type: String
+    },
+    gender: {
+        type: String
+    },
+    age: {
+        type: Number
+    },
+    language: {
+        type: String
+    },
+    email: {
+        type: String
+    },
+    contacts: {
+        type: Array
+    }
+});
+
+// create new model, passing the schema 
+const Friends = mongoose.model('friends', friendSchema);
+
+// SQL 
+
+// create new database
+const sequelize = new Sequelize('database', null, null, {
+    dialect: 'sqlite',
+    storage: './aliens.sqlite',
+});
+
+// define schema 
+
+const Aliens = sequelize.define('aliens', {
+    firstName: { type: Sequelize.STRING },
+    lastName: { type: Sequelize.STRING },
+    planet: { type: Sequelize.STRING },
+});
+
+// forced to sync once server is started 
+
+Aliens.sync({ force: true }).then(() => {
+    _.times(10, (i) => {
+        Aliens.create({
+            firstName: casual.first_name,
+            lastName: casual.last_name,
+            planet: casual.word,
+        });
+    });
+});
+
+export { Friends, Aliens };
---
+++ data/schema.js
@@ -0,0 +1,68 @@
+import { resolvers } from './resolvers';
+import { makeExecutableSchema } from 'graphql-tools';
+
+const typeDefs = `
+ # types of properties you can call in graphql
+
+    type Friend {
+        id: ID
+        firstName: String
+        lastName: String
+        gender: Gender
+        age: Int
+        language: String
+        email: String
+        contacts: [Contact]
+    }
+
+    type Alien {
+        id: ID
+        firstName: String
+        lastName: String
+        planet: String
+    }
+
+    type Contact {
+        firstName: String
+        lastName: String
+    }
+
+    enum Gender {
+        MALE
+        FEMALE
+        OTHER
+    }
+
+    type Query {
+        getFriend(id: ID): Friend
+    }
+
+    # If there is an input type, same fields must be present as the type 
+
+    input FriendInput {
+        id: ID
+        firstName: String!
+        lastName: String
+        gender: Gender
+        age: Int
+        language: String
+        email: String
+        contacts: [ContactInput]
+    } 
+
+    input ContactInput {
+        firstName: String
+        lastName: String 
+    }
+
+    type Mutation {
+        createFriend(input: FriendInput): Friend
+        updateFriend(input: FriendInput): Friend
+        # Return String since the friend won't be in the db anymore
+        deleteFriend(id: ID!): String
+    }
+`;
+
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+export { schema };
